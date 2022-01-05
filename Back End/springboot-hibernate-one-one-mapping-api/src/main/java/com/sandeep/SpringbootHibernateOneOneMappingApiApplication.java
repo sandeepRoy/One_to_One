5891f62c8528b1cf99ee4a010f380ab3192162f0
Sandeep Roy <sandeep.roy2014@gmail.com>
2022-01-05 13:59:25 +0530

    Commit Check

--- Back End/springboot-hibernate-one-one-mapping-api/src/main/java/com/sandeep/SpringbootHibernateOneOneMappingApiApplication.java
+++ Back End/springboot-hibernate-one-one-mapping-api/src/main/java/com/sandeep/SpringbootHibernateOneOneMappingApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootHibernateOneOneMappingApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootHibernateOneOneMappingApiApplication.class, args);
 	}
-
 }
