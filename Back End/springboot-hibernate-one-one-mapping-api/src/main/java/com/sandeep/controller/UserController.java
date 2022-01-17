9424060bc3538997704ba2886d71b011b1ec1f2b
Sandeep Roy <sandeep.roy2014@gmail.com>
2022-01-17 15:13:35 +0530

    Update UserController

--- Back End/springboot-hibernate-one-one-mapping-api/src/main/java/com/sandeep/controller/UserController.java
+++ Back End/springboot-hibernate-one-one-mapping-api/src/main/java/com/sandeep/controller/UserController.java
@@ -3,7 +3,9 @@ package com.sandeep.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sandeep.repository.UserRepository;
 import com.sandeep.entity.User;
-import com.sandeep.entity.UserProfile;
+import com.sandeep.exception.ResourceNotFoundException;
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -19,11 +21,20 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
+	//get all data
 	@GetMapping("/users")
 	public List<User> getAllUsers(){
 		return userRepository.findAll();
 	}
 	
+	//get data by id
+	@GetMapping("/users/{id}")
+	public ResponseEntity<User> getUserById(@PathVariable Long id) {
+		User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No User with id: " + id));
+		return ResponseEntity.ok(user);
+	}
+	
+	// post data
 	@PostMapping("/users")
 	public User createUser(@RequestBody User user) {
 		return userRepository.save(user);
